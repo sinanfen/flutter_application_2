a470f64e31b426686ec6129575356a43d42636cb
Sinan <sinanfen@hotmail.com>
2023-05-04 23:53:53 +0300

    first

--- lib/main.dart
+++ lib/main.dart
@@ -12,42 +12,61 @@ class MyApp extends StatelessWidget {
         "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg";
 
     return MaterialApp(
-      theme: ThemeData(accentColor: Colors.amber),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bu bir title widget'),
-          backgroundColor: Colors.amber,
-        ),
-        body: Center(
-          widthFactor: 3,
-          heightFactor: 3,
-          child: Container(
-            height: 300,
-            width: 300,
-            // alignment: Alignment.center,
-            child: Container(
-              decoration: BoxDecoration(
-                color: const Color(0xff7c94b6),
-                image: const DecorationImage(
-                  image: NetworkImage(_myImgage1),
-                  fit: BoxFit.cover,
+        theme: ThemeData(primaryColor: Colors.teal, accentColor: Colors.purple),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Bu bir title Widget'),
+            backgroundColor: Colors.amber,
+          ),
+          body: Container(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  color: Colors.orangeAccent.shade100,
+                  width: 50,
+                  height: 50,
+                  margin: EdgeInsets.fromLTRB(1, 5, 1, 5),
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    'D',
+                    textAlign: TextAlign.center,
+                  ),
                 ),
-                border: Border.all(width: 8, color: Colors.amber.shade200),
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    topRight: Radius.circular(30)),
-              ),
+                Container(
+                    color: Colors.orangeAccent.shade200,
+                    width: 50,
+                    height: 50,
+                    margin: EdgeInsets.fromLTRB(1, 5, 1, 5),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'A',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                    color: Colors.orangeAccent.shade400,
+                    width: 50,
+                    height: 50,
+                    margin: EdgeInsets.fromLTRB(1, 5, 1, 5),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'R',
+                      textAlign: TextAlign.center,
+                    )),
+                Container(
+                    color: Colors.orangeAccent.shade700,
+                    width: 50,
+                    height: 50,
+                    margin: EdgeInsets.fromLTRB(1, 5, 1, 5),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'T',
+                      textAlign: TextAlign.center,
+                    )),
+              ],
             ),
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            debugPrint('Tıklandı');
-          },
-          child: Icon(Icons.reset_tv, color: Colors.deepPurple),
-          backgroundColor: Colors.amber,
-        ),
-      ),
-    );
+        ));
   }
 }
